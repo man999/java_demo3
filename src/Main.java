4270377f3ecd7c4c8aa0431ad2d050fe865f4eee
man999 <man9992012@ya.ru>
2026-02-17 15:58:08 +0300

    char freq count

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-void main(String[] args) {
-  myConc("Sergey", "Petrov", 40);
- }
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
 
-void myConc(String p1, String p2, int p3) {
-  System.out.println(p1 + " " + p2 + " " + p3);
+void main(String[] args) {
+  //
+  int[] freq = new int[26];
+  char inputChar[];
+  Scanner scanner = new Scanner(System.in);
+  System.out.println("press q for exit");
+  //get and analysing input
+  while (true) {
+    String input = scanner.nextLine();
+    if (input.equals("q")) break;
+    Arrays.fill(freq, 0);
+    inputChar = input.toUpperCase(Locale.ROOT).toCharArray();
+//    System.out.println(input);
+//    System.out.println(inputChar);
+    for (char ch : inputChar) {
+      int dec = (int) ch - 65;
+      if (dec > 0 && dec < 27) freq[ch - 65]++;
+    }
+    //display result
+    for (int i = 0; i < freq.length; i++) {
+      if (freq[i] == 0) continue;
+      System.out.printf("%s:%d\t", (char) (i + 65), freq[i]);
+    }
+    System.out.println("");
+  }
 }
