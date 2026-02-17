ecca1baafacef8167e8caa9a89740f7642b4b8bd
man999 <man9992012@ya.ru>
2026-02-17 16:24:10 +0300

    char freq count 2

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,18 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
-void main(String[] args) {
+void main() {
   //
   int[] freq = new int[26];
-  char inputChar[];
   Scanner scanner = new Scanner(System.in);
   System.out.println("press q for exit");
   //get and analysing input
   while (true) {
     String input = scanner.nextLine();
+    if (input.isEmpty()) continue;
     if (input.equals("q")) break;
     Arrays.fill(freq, 0);
-    inputChar = input.toUpperCase(Locale.ROOT).toCharArray();
-//    System.out.println(input);
-//    System.out.println(inputChar);
+    char[] inputChar = input.toUpperCase(Locale.ROOT).toCharArray();
     for (char ch : inputChar) {
       int dec = (int) ch - 65;
       if (dec > 0 && dec < 27) freq[ch - 65]++;
@@ -25,6 +23,6 @@ void main(String[] args) {
       if (freq[i] == 0) continue;
       System.out.printf("%s:%d\t", (char) (i + 65), freq[i]);
     }
-    System.out.println("");
+    System.out.println();
   }
 }
